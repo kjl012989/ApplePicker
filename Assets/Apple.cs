2f86152e5cff5b5176ec4061fe7713a6910b9f50
kjl012989 <lee.kevinj@outlook.com>
2016-06-06 19:40:12 -0400

    Project Complete

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Apple : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	public static float bottomY = -20f;
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(transform.position.y < bottomY){
+			Destroy (this.gameObject);
+
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+			apScript.AppleDestroyed();
+		}
 	}
 }
